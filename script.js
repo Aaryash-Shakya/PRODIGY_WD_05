b994a74cd231e46e6549bc70e4aa7d9ad400a539
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-10-15 09:57:35 +0545

    feat: add sunRise, sunSet, uvIndex

--- script.js
+++ script.js
@@ -13,12 +13,22 @@ const weather = document.querySelector('#weather')
 const chanceOfRain = document.querySelector('#chanceOfRain')
 const humidity = document.querySelector('#humidity')
 const windSpeed = document.querySelector('#windSpeed')
+const sunRise = document.querySelector('#sunRise')
+const sunSet = document.querySelector('#sunSet')
+const uv = document.querySelector('#uv')
+
+
+// current date
+const today = new Date()
 
 // fetch api
 const fetchData = (location) => {
     fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`)
         .then(res => {
-            if (!res.ok) {
+            if (res.status == 400) {
+                return alert('Invalid Location')
+            }
+            else if (!res.ok) {
                 throw new Error('Network response was not ok')
             }
             return res.json() // Parse the response as JSON
@@ -29,12 +39,36 @@ const fetchData = (location) => {
 
             // Assign data in html
             address.innerText = `${data.location.name}, ${data.location.country}`
-            temperature.innerText =data.current.temp_c
-            feelsLikeTemperature.innerText =data.current.feelslike_c
-            weather.innerText =data.current.condition.text
-            chanceOfRain.innerText =data.current.precip_mm
-            humidity.innerText =data.current.humidity
-            windSpeed.innerText =data.current.wind_kph
+            temperature.innerText = data.current.temp_c
+            feelsLikeTemperature.innerText = data.current.feelslike_c
+            weather.innerText = data.current.condition.text
+            chanceOfRain.innerText = data.current.precip_mm
+            humidity.innerText = data.current.humidity
+            windSpeed.innerText = data.current.wind_kph
+            uv.innerText = data.current.uv
+        })
+        .catch(error => {
+            // Handle errors or exceptions here
+            console.error('There was a problem with the fetch operation:', error)
+        })
+
+    fetch(`https://api.weatherapi.com/v1/astronomy.json?key=${apiKey}&q=${location}&dt=${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`)
+        .then(res => {
+            if (res.status == 400) {
+                return alert('Invalid Location')
+            }
+            else if (!res.ok) {
+                throw new Error('Network response was not ok')
+            }
+            return res.json() // Parse the response as JSON
+        })
+        .then(data => {
+            // Handle the data from the response here
+            console.log(data)
+
+            // Assign data in html
+            sunRise.innerText = data.astronomy.astro.sunrise
+            sunSet.innerText = data.astronomy.astro.sunset
         })
         .catch(error => {
             // Handle errors or exceptions here
@@ -43,18 +77,18 @@ const fetchData = (location) => {
 }
 
 // button event listener
-searchButton.addEventListener('click', ()=>{
-    if(searchText.value===''){
+searchButton.addEventListener('click', () => {
+    if (searchText.value === '') {
         alert("Location can't be empty")
     }
-    else{
+    else {
         fetchData(searchText.value)
     }
 })
 
 // add button trigger with enter key
-searchText.addEventListener('keyup',e=>{
-    if(e.key === 'Enter'){
+searchText.addEventListener('keyup', e => {
+    if (e.key === 'Enter') {
         searchButton.click()
     }
 })
